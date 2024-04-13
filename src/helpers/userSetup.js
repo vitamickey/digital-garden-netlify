eceeaa604964dc3004495fed99f52acfea7d77b0
Michaela Cheong <michaelacheong@gmail.com>
2024-04-13 20:28:04 +1000

    Trying to add a font, fixing CSS stuff

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,7 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  eleventyConfig.addPassthroughCopy("src/site/styles/user/fonts/*")
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
