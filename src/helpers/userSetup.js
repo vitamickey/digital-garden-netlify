816af620c82bc0c5faa97d58859ccfe7a6688396
Michaela Cheong <michaelacheong@gmail.com>
2024-05-06 18:42:05 +1000

    Oh I might actually fix the font this time

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,7 +5,9 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
-  eleventyConfig.addPassthroughCopy("src/site/styles/user/fonts/*.woff");
+  eleventyConfig.addPassthroughCopy("src/site/styles/user/fonts/Black/*");
+  eleventyConfig.addPassthroughCopy("src/site/styles/user/fonts/Bold/*");
+  eleventyConfig.addPassthroughCopy("src/site/styles/user/fonts/BoldItalic/*");
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
